27c8201d0d1bcff98c7f846735e4b073480b95fb
kanae rinna <66352988+lina1280187381@users.noreply.github.com>
2020-06-30 10:26:52 +0800

    最終バージョン：結果を１回だけ出力

---
+++ judgement/test/src/judgementTest.cpp
@@ -0,0 +1,165 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  judgementTest.cpp
+ * @brief ModuleDescription
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "judgementTest.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* judgement_spec[] =
+  {
+    "implementation_id", "judgementTest",
+    "type_name",         "judgementTest",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "ye-linna",
+    "category",          "data",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+judgementTest::judgementTest(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_data_inIn("data_in", m_data_in)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+judgementTest::~judgementTest()
+{
+}
+
+
+
+RTC::ReturnCode_t judgementTest::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  
+  // Set OutPort buffer
+  addOutPort("data_in", m_data_inOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t judgementTest::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t judgementTest::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t judgementTest::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t judgementTest::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t judgementTest::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t judgementTest::onExecute(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t judgementTest::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t judgementTest::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t judgementTest::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t judgementTest::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t judgementTest::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void judgementTestInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(judgement_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<judgementTest>,
+                             RTC::Delete<judgementTest>);
+  }
+  
+};
+
+
